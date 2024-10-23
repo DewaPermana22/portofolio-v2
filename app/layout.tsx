46ee15c9e10a8e9b24862124865a2b393de020a7
DewaPermana22 <dewarisa77@gmail.com>
2024-10-23 22:00:11 +0700

    Fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/ui/Header";
 import Transisi from "@/components/Transisi";
--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
 import Navbar from '../Navbar'
+import Image from 'next/image'
 function Header() {
   return (
     <header className='flex relative -top-10 justify-between items-center '>
     <Link href="/" >
-       <img className='w-32 xl:w-44' src="/Dewapermana..svg" alt="" />
+    <Image
+    src ="/Dewapermana..svg"
+    alt='logo'
+    className='w-36 xl:w-44'  
+    width="200"
+    height="200"/>
     </Link>
   <Navbar/>    
     </header>
